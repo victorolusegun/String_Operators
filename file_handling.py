7d71575e75bdabb9022f58128014e134de7df77a
Victor Olusegun <victorolusegun003@gmail.com>
2025-08-04 11:41:08 +0100

    fixed issues

--- file_handling.py
+++ file_handling.py
@@ -11,41 +11,41 @@
 
 #Using a context manager. You have access to the file variable outside the function but the file is closed
 # READING
-# with open('file_handling.txt', 'r') as file:
+with open('file_handling.txt', 'r') as file:
 #.read() prints out the entire contenrs of the file
-    # file_contents = file.read()
-    # print(file_contents)
+    file_contents = file.read()
+    print(file_contents)
 #.readline() prints out the first line of the file
-    # file_oneline = file.readline()
-    # print(file_oneline, end ='')
+    file_oneline = file.readline()
+    print(file_oneline, end ='')
 # #repeating it prints out the next line
-    # file_oneline = file.readline()
-    # print(file_oneline, end = '')
+    file_oneline = file.readline()
+    print(file_oneline, end = '')
 #the next block of code is a more efficient way to print out the contents of a file
-    # for line in file:
-    #     print(line, end= '')
+    for line in file:
+        print(line, end= '')
 #the next block of code is essential to the next 3 after it
-    # size_to_read = 50
-    # file_content = file.read(size_to_read)
-    # print(file_content, end = '')
+    size_to_read = 50
+    file_content = file.read(size_to_read)
+    print(file_content, end = '')
 #.seek() tells the file what position to start printing from
-    # file.seek(10)
-    # file_content = file.read(size_to_read)
-    # print(file_content, end = '')
+    file.seek(10)
+    file_content = file.read(size_to_read)
+    print(file_content, end = '')
 #.tell() prints out character the .read() is currently at
-    # print(file.tell())
+    print(file.tell())
 #another efficient way of reading files
-    # while len(file_content) > 0:
-    #     print(file_content, end = '')
-    #     file_content = file.read(size_to_read)
+    while len(file_content) > 0:
+        print(file_content, end = '')
+        file_content = file.read(size_to_read)
 
 # WRITE
-# with open('new_file.txt', 'w') as f:
-#     f.write('''Artificial Intelligence is transforming the world.
-# From healthcare to finance, AI is finding real-world applications.
-# Natural Language Processing is a major branch of AI.
-# It allows machines to understand and generate human language.
-# Are you ready to explore what comes next?''') 
+with open('new_file.txt', 'w') as f:
+    f.write('''Artificial Intelligence is transforming the world.
+From healthcare to finance, AI is finding real-world applications.
+Natural Language Processing is a major branch of AI.
+It allows machines to understand and generate human language.
+Are you ready to explore what comes next?''') 
 
 #Reading from one file and writing to another file
 with open('file_handling.txt', 'r') as file:
